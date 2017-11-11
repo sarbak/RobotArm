58f0ba2516da2718af0a4afb705426f4500c9ccb
Emre Sarbak <emre.sarbak@gmail.com>
2017-11-11 16:20:51 -0600

    Three-joint structure
    It has three potentiometers on the breadboard that control the three joints

--- RobotArm_-_Initial_Testing/RobotArm_-_Initial_Testing.ino
+++ RobotArm_-_Initial_Testing/RobotArm_-_Initial_Testing.ino
@@ -2,23 +2,28 @@
 
 Servo servo_bottom;      //initialize a servo object for the connected servo  
 Servo servo_middle;
+Servo servo_top;
 
 int servo_bottom_output = 9;
 int servo_middle_output = 11;
+int servo_top_output = 13;
 
 int angle_bottom = 0;    
 int angle_middle = 0;
+int angle_top = 0;
     
-int potentio_bottom = A0;      // initialize the A0analog pin for potentiometer
-int potentio_middle = A5;      // initialize the A0analog pin for potentiometer
+int potentio_bottom = A0;      // initialize the pin for potentiometer
+int potentio_middle = A2;      // initialize the pin for potentiometer
+int potentio_top = A5;      // initialize the pin for potentiometer
 
 void setup() 
 {
    
  Serial.begin(9600);
  
- servo_bottom.attach(servo_bottom_output);   // attach the signal pin of servo to pin9 of arduino
- servo_middle.attach(servo_middle_output);   // attach the signal pin of servo to pin9 of arduino
+ servo_bottom.attach(servo_bottom_output);   // attach the signal pin of servo to pin
+ servo_middle.attach(servo_middle_output);   // attach the signal pin of servo to pin
+ servo_top.attach(servo_top_output);   // attach the signal pin of servo to pin
 
 } 
  
@@ -27,11 +32,13 @@ void loop()
    
  angle_bottom = map(analogRead(potentio_bottom), 0, 1023, 60, 120);     // scaling the potentiometer value to angle value for servo
  angle_middle = map(analogRead(potentio_middle), 0, 1023, 30, 150);     // scaling the potentiometer value to angle value for servo
+ angle_top = map(analogRead(potentio_top), 0, 1023, 30, 150);  // scaling the potentiometer value to angle value for servo
 
- Serial.println(angle_middle);
+ //Serial.println(angle_middle);
  
  servo_bottom.write(angle_bottom);                   //command to rotate the servo to the specified angle 
  servo_middle.write(angle_middle);
+ servo_top.write(angle_top);
  
  delay(5);             
 
